d7645db508d9e1a747de5673f88deb4f7c3e54be
Wai Fun Leong <36015297+Wai-Fun@users.noreply.github.com>
2026-07-09 13:57:33 -0400

    Update Beginner - 4. Having Vs WHERE.sql

--- Beginner - 4. Having Vs WHERE.sql
+++ Beginner - 4. Having Vs WHERE.sql
@@ -1,1 +1,23 @@
+/*
+- Both HAVING AND WHERE are used to filter data 
+- WHERE filter rows (individual rows) before GROUP BY 
+- HAVING filter aggregated groups after group by 
+*/
 
+SELECT visit_type, COUNT(*) AS total_visits
+FROM patient_metrics
+GROUP BY visit_type
+HAVING COUNT(*) > 5;
+
+SELECT visit_type, COUNT(*) AS total_visits
+FROM patient_metrics
+WHERE bmi > 30
+GROUP BY visit_type
+HAVING COUNT(*) > 3;
+
+-- example: Find payers where insurance visits over $100,000 happened more than 3 times
+SELECT payer_type, COUNT(*) AS expensive_visits
+FROM patient_billing
+WHERE treatment_cost > 100000
+GROUP BY payer_type
+HAVING COUNT(*) > 3;
