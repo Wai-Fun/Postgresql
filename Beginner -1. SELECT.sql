c3b066e9f4f76bbb08400c10d7100898390517e8
Wai Fun Leong <36015297+Wai-Fun@users.noreply.github.com>
2026-07-09 12:55:35 -0400

    Update Beginner -1. SELECT.sql

--- Beginner -1. SELECT.sql
+++ Beginner -1. SELECT.sql
@@ -1,1 +1,23 @@
+-- SELECT everthing (all the columns) from a table
+SELECT *
+FROM patient_demographics
+  
 
+-- SELECT a specific column from a table
+SELECT patient_id
+FROM patient_demographics;
+
+
+-- SELECT 2 specific columns from a table
+SELECT patient_id, gender
+FROM patient_demographics;
+
+
+-- Use SELECT to reorder the columns in the table
+SELECT gender, age, birth_date, first_name, last_name, patient_id
+FROM patient_demographics;
+
+
+-- Use DISTICT for only unique value
+SELECT DISTINCT gender
+FROM patient_demographics;
