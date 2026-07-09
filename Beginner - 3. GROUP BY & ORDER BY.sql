268995c7c7880bcc8d671093bd3f2de74290e4de
Wai Fun Leong <36015297+Wai-Fun@users.noreply.github.com>
2026-07-09 13:49:15 -0400

    Update Beginner - 3. GROUP BY & ORDER BY.sql

--- Beginner - 3. GROUP BY & ORDER BY.sql
+++ Beginner - 3. GROUP BY & ORDER BY.sql
@@ -9,28 +9,34 @@ SELECT gender, COUNT(gender)
 FROM patient_demographics
 GROUP BY gender;
 
+
 SELECT gender, avg(age), MIN(age), MAX(age)
 FROM patient_demographics
 GROUP BY gender;
 
+
 SELECT payer_type, SUM(treatment_cost) AS total_cost
 FROM patient_billing
 GROUP BY payer_type;
+
 
 -- ORDER BY (sort accending)
 SELECT *
 FROM patient_demographics
 ORDER BY age;
 
+
 -- ORDER BY and DESC (sort decending)
 SELECT *
 FROM patient_demographics
 ORDER BY age DESC;
 
+
 -- ORDER BY multiple columns
 SELECT *
 FROM patient_demographics
 ORDER BY gender, age;  
+
 
 -- GROUP BY & ORDER BY, with DESC and LIMIT
 SELECT visit_type, COUNT(*) AS total_visits
